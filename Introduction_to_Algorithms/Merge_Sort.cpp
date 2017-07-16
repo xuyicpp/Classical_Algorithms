3ac5923adefd8d0ab5c4540c456ded579b8d9b6d
徐意 <540767457@qq.com>
2017-07-16 22:25:24 +0800

    test

--- Introduction_to_Algorithms/Merge_Sort.cpp
+++ Introduction_to_Algorithms/Merge_Sort.cpp
@@ -3,9 +3,39 @@
 
 using namespace std;
 
+void merge(vector A, int p, int q, int r);
+void merge_sort(vector A, int p, int r);
+
 int main()
 {
-	
+	vector<int> v={5,4,3,2,1,0};
 
+	cout<<"Before sort:";
+	for(auto q : v)
+		cout<<q<<" ";
+	cout<<endl;
+
+
+
+	cout<<"After sort:";
+	for(auto q : v)
+		cout<<q<<" ";
+	cout<<endl;
+			
 	return 0;
+}
+
+void merge(vector A, int p, int q, int r)
+{
+	
+	
+	return;
+}
+
+
+void merge_sort(vector A, int p, int r)
+{
+
+
+	return;
 }
