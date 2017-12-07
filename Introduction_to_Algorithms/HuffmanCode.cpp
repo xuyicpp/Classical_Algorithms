85c6c0989f11542d9e5ca833f0c7abc50f41cdef
小徐 <yixu_cs@hust.edu.cn>
2017-12-07 17:10:39 +0800

    test

--- Introduction_to_Algorithms/HuffmanCode.cpp
+++ Introduction_to_Algorithms/HuffmanCode.cpp
@@ -1,6 +1,19 @@
+/**
+ * @author XY
+ * @date 20171207
+ */
+
 #include <iostream>
 
 using namespace std;
+
+class HuffmanTree
+{
+public:
+	HuffmanTree();
+	~HuffmanTree();
+	
+};
 
 int main()
 {
