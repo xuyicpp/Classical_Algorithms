d21601309cfb8da91a10b7c017432ed98c29c2a4
小徐 <yixu_cs@hust.edu.cn>
2017-12-08 20:41:25 +0800

    Revert "test"
    This reverts commit 85c6c0989f11542d9e5ca833f0c7abc50f41cdef.

--- Introduction_to_Algorithms/HuffmanCode.cpp
+++ Introduction_to_Algorithms/HuffmanCode.cpp
@@ -1,19 +1,6 @@
-/**
- * @author XY
- * @date 20171207
- */
-
 #include <iostream>
 
 using namespace std;
-
-class HuffmanTree
-{
-public:
-	HuffmanTree();
-	~HuffmanTree();
-	
-};
 
 int main()
 {
